716dde7e9bba5bde964e3ff487871d0710a5fa7c
Bhargodev Arya <bhargodevarya@gmail.com>
2020-07-06 02:45:42 +0530

    Documentation for hw

--- hw.erl
+++ hw.erl
@@ -1,8 +1,14 @@
+%% module declaration, similar to class decalarations
+%% Every statement ends with a .
 -module(hw).
 
+%% Export statment similar to nodejs
+%% [] means a list <functionName/num_of_params_it_accepts>
 -export([helloworld/0, helloworld/1]).
 
+%% 2 overloaded functions
 helloworld() -> "Hello World again".
 
+%% Variabled must start with Cap letter
 helloworld(X) ->
   "Hello " ++ X.
